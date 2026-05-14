449f4bcaf2db95f3956f42f45c71832a1a3b46ff
Indrani Mukhopadhyay <indranimukhopadhyay17@gmail.com>
2026-05-14 19:50:43 +0530

    Small fixes

--- app/hotels/page.tsx
+++ app/hotels/page.tsx
@@ -632,7 +632,92 @@ toast.error(
       </div>
 
       {/* HOTELS */}
+{/* LOADING */}
 
+{loading && (
+
+  <div
+    style={{
+
+      minHeight:"45vh",
+
+      display:"flex",
+
+      alignItems:"center",
+
+      justifyContent:"center",
+
+      flexDirection:"column",
+
+      gap:"18px",
+
+    }}
+  >
+
+    <div
+      style={{
+
+        width:"55px",
+
+        height:"55px",
+
+        border:"5px solid #f3f3f3",
+
+        borderTop:
+          "5px solid #8B0000",
+
+        borderRadius:"50%",
+
+        animation:
+          "spin 1s linear infinite",
+
+      }}
+    />
+
+    <h2
+      style={{
+
+        color:"#8B0000",
+
+        fontSize:"18px",
+
+        fontWeight:700,
+
+      }}
+    >
+
+      Loading Hotels...
+
+    </h2>
+
+  </div>
+
+)}
+
+{/* EMPTY */}
+
+{!loading &&
+ hotels.length === 0 && (
+
+  <p
+    style={{
+
+      textAlign:"center",
+
+      marginTop:"40px",
+
+      color:"#777",
+
+      fontSize:"16px",
+
+    }}
+  >
+
+    No hotels found
+
+  </p>
+
+)}
       <div className="card-list">
 
         {hotels.map((h)=>(
